95142c9c8158662d43e2d0919bc03d86b8622436
tomsnews <49147550+tomsnews@users.noreply.github.com>
2020-11-06 15:20:45 +0100

    Build

---
+++ build/precache-manifest.15555ab664a95da92ded555dbdb35c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1e4c2eca2716a6dd35e90e39281b99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e328d407a9b19ab9e480",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "e4c67fe219681b1a64e4",
+    "url": "/static/css/main.94e625be.chunk.css"
+  },
+  {
+    "revision": "e328d407a9b19ab9e480",
+    "url": "/static/js/2.786e7884.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.786e7884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c67fe219681b1a64e4",
+    "url": "/static/js/main.fe86dac0.chunk.js"
+  },
+  {
+    "revision": "84aebeada1cc75b4439d",
+    "url": "/static/js/runtime-main.e6a923cf.js"
+  }
+]);
